48130bac1c9816c29fe435bccfa1b5986a02fbfc
brianhong <brianhong0510@gmail.com>
2018-11-16 21:04:24 -0500

    Cats on the screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, PureComponent } from 'react';
 import './App.css';
+var parseString = require('xml2js').Parser({ explicitArray: false }).parseString;
+
+const imagesLink = "https://cors-anywhere.herokuapp.com/http://thecatapi.com/api/images/get?format=xml&results_per_page=25";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      cats: [],
+      favorites: []
+    }
+    this.toggleFavoriteFact = this.toggleFavoriteFact.bind(this);
+  }
+
+  componentDidMount() {
+    window.fetch(imagesLink)
+      .then(res => res.text())
+      .then(
+        (xmlString) => {
+          parseString(xmlString, (err, { response }) => {
+            if (err) {
+              console.error("Uh oh!");
+            }
+            const imagesObject = response;
+            const images = imagesObject.data.images.image;
+            const initImages = images.map((image, index) => {
+              return {
+                id: index,
+                imageSource: image.url
+              }
+            });
+            this.setState({
+              isLoaded: true,
+              cats: initImages
+            })
+          });
+        },
+        (error) => {
+          this.setState({
+            error: true
+          })
+        }
+      )
+  }
+
+  toggleFavoriteFact(factId) {
+    const updatedFavoritesList = this.state.favorites.includes(factId) ?
+      this.state.favorites.filter(favorited => favorited.id !== factId) :
+      [...this.state.favorites, factId];
+
+    this.setState((state, props) => {
+      return {
+        favorites: updatedFavoritesList
+      }
+    });
+  }
+
   render() {
+    const { cats } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.isLoaded ? <Test cats={cats} /> : <div>HelloWOrld</div>}
       </div>
+    );
+  }
+}
+
+class Test extends PureComponent {
+  render() {
+    const { cats } = this.props;
+    return (
+      cats.map(cat => {
+        return (
+          <div>
+            <div>{cat.id + 1}</div>
+            <img alt="" src={cat.imageSource}/>
+          </div>
+        )
+      })
     );
   }
 }
